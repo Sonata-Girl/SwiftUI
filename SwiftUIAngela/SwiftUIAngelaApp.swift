d3bab084137dcd5256066871c2613693fef588ad
SonataGirl <mike@magic.org>
2023-05-09 21:37:22 +0300

    Initial Commit

---
+++ SwiftUIAngela/SwiftUIAngelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAngelaApp.swift
+//  SwiftUIAngela
+//
+//  Created by Sonata Girl on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAngelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
