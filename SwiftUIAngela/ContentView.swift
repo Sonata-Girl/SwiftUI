e2c901ce3e51b41ac03425f5a6f82f081b08d5cf
SonataGirl <mike@magic.org>
2023-06-04 22:30:03 +0300

    add start app

--- SwiftUIAngela/ContentView.swift
+++ SwiftUIAngela/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+         ZStack {
+             Color(red: 0.20, green: 0.63, blue: 0.52, opacity: 1.00)
+                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+             VStack {
+                 VStack {
+                     Image("ConataComp2").resizable()
+                         .aspectRatio(contentMode: .fit)
+                         .frame(width: 300)
+                         .clipShape(Circle())
+                         .overlay(
+                            Circle().stroke(Color.white,lineWidth: 5))
+                     Text("Sonata")
+                        .font(Font.custom("Kanit-Medium", size: 50))
+                        .bold()
+                        .foregroundColor(.white)
+                     Text("iOS Delivopter")
+                         .foregroundColor(.white)
+                         .font(.system(size: 20))
+                     Divider()
+                     RoundedRectangle(cornerRadius: 30)
+                         .fill(Color.white)
+                         .frame(height: 50)
+                         .foregroundColor(.white)
+                         .overlay(Text("+ХХ УУУ ЕЕЕ ТТТ ООО").foregroundColor(.black))
+                 }
+                 
+             }
         }
-        .padding()
     }
 }
 
